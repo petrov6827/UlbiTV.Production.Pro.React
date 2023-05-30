04bdf93bc048b2b75389a2f8fa5980421112ca9c
dimon <petrov6827@gmail.com>
2023-05-31 01:35:58 +0300

    переключение тем светлая темная, хук useTheme с помощъю provider content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import { Suspense, useState } from "react"
+import './styles/index.scss';
+import { Suspense, useContext, useState } from "react"
 import { Link, Route, Routes } from "react-router-dom"
 import { Loading } from "./components/Loading/Loading"
 import AboutPageAsync from "./pages/About/AboutPage.async"
 import MainPageAsync from "./pages/MainPage/MainPage.async"
-import './styles/index.scss';
-
-export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark',
-}
+import { Theme, ThemeContext } from "./styles/Theme/ThemeContext"
+import { useTheme } from './styles/Theme/useTheme';
 
 export const App = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
-
-    const toggleTheme = setTheme(Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+    const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={`app ${theme}`}>
             <Link to='/about'>About</Link>
             <Link to='/'>MainPage</Link>
 
-            {/* <button onClick={toggleTheme}>{Theme.LIGHT ? 'Темная тема' : 'Светлая тема'}</button> */}
+            <button onClick={toggleTheme}>Цвет темы</button>
 
             <Suspense fallback={<Loading />}>
                 <Routes>
