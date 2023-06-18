57289f8b11d167ab341505432306a5657b1fcd56
dimon <petrov6827@gmail.com>
2023-06-18 17:25:22 +0300

    тесты для classNames

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        
+        //в доке jestjs.io вкладка typescript
+        '@babel/preset-typescript',
+    ],
+};
