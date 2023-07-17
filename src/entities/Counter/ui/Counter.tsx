dc5cebf163bc5f4c056f603658162cfb2056525e
dimon <petrov6827@gmail.com>
2023-07-17 23:58:37 +0300

    Counter model

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,5 +1,19 @@
-export const Counter = () => (
-	<div>
-		<h1>value</h1>
-	</div>
-)
+import { Button } from "shared/ui/Button/Button"
+
+export const Counter = () => {
+
+	const increm = () => {}
+	const decrem = () => {}
+
+	return (
+		<div>
+			<h1>value</h1>
+			<Button onClick={increm}>
+            in
+			</Button>
+			<Button onClick={decrem}>
+            de
+			</Button>
+		</div>
+	)
+}
